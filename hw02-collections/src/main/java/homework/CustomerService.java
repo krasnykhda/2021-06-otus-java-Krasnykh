b4dcd15b4f482f1c15f8fa0508ae06b2880fe0c2
Dmitry Krasnykh <krasnykhda@gmail.com>
2021-07-17 14:57:16 +0300

    Улушил создание копии. Вместо всей коллекции теперь копируется только нужная запись.

--- hw02-collections/src/main/java/homework/CustomerService.java
+++ hw02-collections/src/main/java/homework/CustomerService.java
@@ -9,21 +9,23 @@ import java.util.TreeMap;
 
 public class CustomerService {
     private final NavigableMap<Customer, String> treeMap = new TreeMap<>((o1, o2) -> (int) (o1.getScores() - o2.getScores()));
-    public NavigableMap<Customer, String> copyTreeMap(){
+    public NavigableMap<Customer, String> copyTreeMap(Map.Entry<Customer, String> entry){
         NavigableMap<Customer, String> treeMapCopy = new TreeMap<>((o1, o2) -> (int) (o1.getScores() - o2.getScores()));
-        for(Map.Entry<Customer, String> entry : treeMap.entrySet()) {
+        if(entry!=null){
             treeMapCopy.put(new Customer(entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()), entry.getValue());
         }
         return treeMapCopy;
     }
     public Map.Entry<Customer, String> getSmallest() {
-        NavigableMap<Customer, String> treeMapCopy = copyTreeMap();
+        Map.Entry<Customer, String> entry = treeMap.firstEntry();
+        NavigableMap<Customer, String> treeMapCopy = copyTreeMap(entry);
         return treeMapCopy.firstEntry();
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        NavigableMap<Customer, String> treeMapCopy = copyTreeMap();
-        return treeMapCopy.higherEntry(customer);
+        Map.Entry<Customer, String> entry = treeMap.higherEntry(customer);
+        NavigableMap<Customer, String> treeMapCopy = copyTreeMap(entry);
+        return treeMapCopy.firstEntry();
     }
 
     public void add(Customer customer, String data) {
