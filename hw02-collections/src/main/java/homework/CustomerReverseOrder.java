c8781b19b7af3b12226ed8f86496587ca092697f
Dmitry Krasnykh <krasnykhda@gmail.com>
2021-07-17 14:16:25 +0300

    Удалил конструктор для стека.

--- hw02-collections/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-collections/src/main/java/homework/CustomerReverseOrder.java
@@ -7,11 +7,7 @@ import java.util.Deque;
 import java.util.TreeMap;
 
 public class CustomerReverseOrder {
-    private final Deque<Customer> stack;
-
-    public CustomerReverseOrder() {
-        this.stack = new ArrayDeque<>();
-    }
+    private final Deque<Customer> stack = new ArrayDeque<>();
 
     public void add(Customer customer) {
         stack.push(customer);
