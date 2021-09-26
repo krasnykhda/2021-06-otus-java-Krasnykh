281a8b31460d85dbcf8c9729e2fd3e47342829d9
dmitry <krasnykhda@gmail.com>
2021-09-26 12:11:51 +0300

    Commit7

--- hw09-jdbc/src/main/java/ru/otus/HomeWork.java
+++ hw09-jdbc/src/main/java/ru/otus/HomeWork.java
@@ -15,12 +15,12 @@ import ru.otus.jdbc.mapper.*;
 import javax.sql.DataSource;
 
 public class HomeWork {
-    /*private static final String URL = "jdbc:postgresql://localhost:5432/demodb"; -- локальный Postgres
+    private static final String URL = "jdbc:postgresql://localhost:5432/demodb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "123";*/
-    private static final String URL = "jdbc:postgresql://localhost:5430/demoDB";
+    private static final String PASSWORD = "123";
+    /*private static final String URL = "jdbc:postgresql://localhost:5430/demoDB";
     private static final String USER = "usr";
-    private static final String PASSWORD = "pwd";
+    private static final String PASSWORD = "pwd";*/
 
     private static final Logger log = LoggerFactory.getLogger(HomeWork.class);
 
