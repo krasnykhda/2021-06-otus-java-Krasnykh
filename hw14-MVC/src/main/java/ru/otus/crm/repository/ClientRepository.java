1eeb129fd75bdfb4d2f7415077992d84cf3c3c46
dmitry <krasnykhda@gmail.com>
2021-11-09 14:38:18 +0300

    hw14-commit6

--- hw14-MVC/src/main/java/ru/otus/crm/repository/ClientRepository.java
+++ hw14-MVC/src/main/java/ru/otus/crm/repository/ClientRepository.java
@@ -6,18 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import ru.otus.crm.model.Client;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface ClientRepository extends CrudRepository<Client, Long> {
-
-
-   Optional<Client> findByName(String name);
-
-    @Query("select * from client where upper(name) = upper(:name)")
-    Optional<Client> findByNameIgnoreCase(@Param("name") String name);
-
-    @Modifying
-    @Query("update client set name = :newName where id = :id")
-    void updateName(@Param("id") long id, @Param("newName") String newName);
+    @Override
+    List<Client> findAll();
 }
