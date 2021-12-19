0bbcaf95791cd4bba95e450ec8c3fdda0bf71e52
dmitry <krasnykhda@gmail.com>
2021-12-19 17:41:13 +0300

    hw16-commit1

--- hw16-MessageSystem/MessageSystemApp/src/main/java/ru/otus/MessageSystemAPP/db/handlers/SaveClientHandler.java
+++ hw16-MessageSystem/MessageSystemApp/src/main/java/ru/otus/MessageSystemAPP/db/handlers/SaveClientHandler.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.otus.MessageSystemAPP.dto.ClientData;
-import ru.otus.MessageSystemAPP.dto.UserData;
 import ru.otus.MessageSystemAPP.crm.service.DBServiceClient;
 import ru.otus.RequestHandler;
 import ru.otus.client.ResultDataType;
